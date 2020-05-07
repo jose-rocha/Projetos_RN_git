5edbe4d492f7119e93d3b76d37c633f70113a101
José Rocha <joserocha@pop-os.localdomain>
2020-05-07 10:35:21 -0300

    Beta

--- AULAS RN SUJEITO ROGRAMADOR/Trabalhando com Banco de Dados/realm/src/Jobs/index.js
+++ AULAS RN SUJEITO ROGRAMADOR/Trabalhando com Banco de Dados/realm/src/Jobs/index.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react'
 import { Text, View } from 'react-native'
+import {Container, Nome, Cargo, CenterView, EditItem, EditText} from './styles';
 
 export default class Jobs extends Component {
     render() {
         return (
-            <View>
-                <Text> {this.props.data.nome} </Text>
-                <Text> {this.props.data.cargo} </Text>
-            </View>
+            <Container>
+                <Nome>{this.props.data.nome} </Nome>
+                <Cargo>{this.props.data.cargo} </Cargo>
+
+                <CenterView>
+                    <EditItem onPress={()=> {}}>
+                        <EditText>Editar</EditText>
+                    </EditItem>
+                    <EditItem onPress={()=> {}} >
+                        <EditText>Excluir</EditText>
+                    </EditItem>
+                </CenterView>
+            </Container>
         )
     }
 }
--- AULAS RN SUJEITO ROGRAMADOR/Trabalhando com Banco de Dados/realm/src/index.js
+++ AULAS RN SUJEITO ROGRAMADOR/Trabalhando com Banco de Dados/realm/src/index.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
-import {Platform, View, Text, StatusBar, StyleSheet } from 'react-native';
+import {Platform, View, Text, StatusBar, Keyboard} from 'react-native';
 
 import {Container, Title, Input, Botao, BotaoText, CenterView, List} from './styles';
-import Jobs from './Jobs'
+import getRealm from './services/realm';
+
+import Jobs from './Jobs';
+
 
 
 export default class App extends Component{
@@ -11,10 +14,54 @@ export default class App extends Component{
     super(props);
     this.state = {
       nome:'',
-      Cargo: '',
+      cargo: '',
       jobs: []
-    }
+    };
+    this.addJob = this.addJob.bind(this);
+    this.saveJob = this.saveJob.bind(this);
+
   }
+
+  saveJob = async (data)=> {
+    const realm = await getRealm();
+
+    const id = realm.objects('Job').length + 1; //gerando id
+    const dadosJobs = {
+      id: id,
+      nome: data.nome,
+      cargo: data.cargo
+    };
+
+    realm.write(()=>{
+      realm.create('Job', dadosJobs);
+    });
+
+  }
+  
+
+addJob = async ()=> {
+  try {
+    if(this.state.nome === '' || this.state.cargo === ''){
+      alert('Por favor preencha todos os campos!')
+      return;
+    }
+
+    const data = {nome: this.state.nome, cargo: this.state.cargo};
+    await this.saveJob(data);
+    let state = this.state;
+    state.nome = '', //nessa linha ele apaga o nome escrito de qu apertar no cadastrar
+    state.cargo = ''; //nessa linha ele apaga o cargo escrito de qu apertar no cadastrar
+    this.setState(state);
+    Keyboard.dismiss(); // //nessa linha serve para fechar o teclado após cadastrar.
+
+
+  } catch (err) {
+    alert(err);
+  }
+}
+
+
+
   render(){
     return (
       <Container>
@@ -27,7 +74,7 @@ export default class App extends Component{
             
             <CenterView>
             <Botao>
-                <BotaoText>Cadastrar</BotaoText>
+                <BotaoText onPress={this.addJob} >Cadastrar</BotaoText>
             </Botao>
 
             <Botao>

511ec038a3d34c5d7749d7b8b7092af608e689cc
José Rocha <josemarlerocha@hotmail.com>
2020-11-30 21:43:42 -0300

    Sistema de pesquisa implementado e funcionando no sistema MD

--- MD/MD NodeJS/JS/Search.js
+++ MD/MD NodeJS/JS/Search.js
@@ -1,36 +1,15 @@
-(function (win, doc) {
-    'use strict';
+document.getElementById('search').addEventListener('keyup', pesquisaTabela());
 
-    let dadosTabela = doc.querySelector('#dadosTabela').childNodes;
-    let Search = doc.querySelector('#Search');
-    let filterKeyword = doc.createElement('tr');
+function pesquisaTabela() {
+    let filter, table, tr, td, i;
+    table = document.getElementById('dados');
+    return function () {
+        tr = table.querySelectorAll("tbody tr");
+        filter = this.value.toUpperCase();
+        for (i = 0; i < tr.length; i++) {
+            let match = tr[i].innerHTML.toUpperCase().indexOf(filter) > -1;
+            tr[i].style.display = match ? "" : "none";
 
-
-    //     try {
-    //         for (let i = 0; i < length; i++) {
-    //             if (dadosTabela.nodeType != 1 && null != dadosTabela.nextSibling) {
-    //                 if (dadosTabela.nextSibling.innerText == filterKeyword) {
-    //                     let newtd = doc.createElement('td');
-    //                     let newLiText = document.createTextNode(filterKeyword);
-    //                     newtd.appendChild(newLiText);
-    //                 }
-    //             }
-    //         }
-
-    //         newFilteredUl.appendChild(newtd)
-    //     } catch (e) {
-    //         console.error(e)
-    //     }
-    //     return newFilteredUl
-    // }
-
-    Search.addEventListener('keyup', function () {
-        let Search = doc.querySelector('#Search').value;
-
-
-    }, false);
-
-
-})(window, document);
-
-// length = dadosTabela.length;
+        }
+    }
+}
